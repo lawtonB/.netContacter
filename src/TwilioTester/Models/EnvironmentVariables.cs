2e20126aefe4a4f7cda12776ac66703a516c87f2
lawtonB <lawton.a.browning@gmail.com>
2016-05-09 19:54:26 -0700

    clean up code and project

---
+++ src/TwilioTester/Models/EnvironmentVariables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwilioTester.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string AccountSid = "ACb0bfd5d796c5ccb460981071eaef4872";
+        public static string AuthToken = "b172bd7438ac4ec16fef767540d8b538";
+        public static string TwilioPhone = "+19713402408";
+    }
+}
